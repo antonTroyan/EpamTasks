5a5ef3c9e64c58d4b6e93a9979c0b0e2e6599277
antonTroyan <32644076+antonTroyan@users.noreply.github.com>
2017-10-13 14:10:40 +0300

    minor changes

--- src/by/troyan/epam/task1/test.java
+++ src/by/troyan/epam/task1/test.java
@@ -5,19 +5,13 @@ import by.troyan.epam.task1.exception.FileIsEmptyException;
 import by.troyan.epam.task1.exception.FileNotExistExeption;
 import by.troyan.epam.task1.exception.NoFileNameException;
 
-import java.io.File;
-import java.io.IOException;
 
 
 public class test {
     public static void main(String[] args) throws FileIsEmptyException, NoFileNameException, FileNotExistExeption {
-//        EntityCreator entityCreator = new EntityCreator("data//triangleData.txt");
-//        entityCreator.fillTriangleList();
-//        System.out.println(entityCreator.getTriangleByIndex(0));
-
-
-
-
+        EntityCreator entityCreator = new EntityCreator("data//triangleData.txt");
+        entityCreator.fillTriangleList();
+        System.out.println(entityCreator.getTriangleByIndex(0));
 
     }
 }
