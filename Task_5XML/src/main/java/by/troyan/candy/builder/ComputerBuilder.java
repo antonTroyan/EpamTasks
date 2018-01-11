41533bef0069f19cb7576d446dcd5581448106a1
antonTroyan <32644076+antonTroyan@users.noreply.github.com>
2018-01-11 12:52:29 +0300

    minor changes

---
+++ Task_5XML/src/main/java/by/troyan/candy/builder/ComputerBuilder.java
@@ -0,0 +1,18 @@
+package by.troyan.candy.builder;
+
+abstract class ComputerBuilder {
+    protected Computer computer;
+
+    public Computer getComputer() {
+        return computer;
+    }
+
+    public void createNewComputer() {
+        computer = new Computer();
+    }
+
+    public abstract void buildSystemBlock();
+    public abstract void buildDisplay();
+    public abstract void buildManipulators();
+
+}
