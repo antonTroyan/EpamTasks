416386e720476b3dfa2cb24f0a493235de54f66b
antonTroyan <32644076+antonTroyan@users.noreply.github.com>
2018-01-13 21:11:43 +0300

    all done

--- Task_5xml/src/test/java/by/troyan/builder/XMLReaderTest.java
+++ Task_5xml/src/test/java/by/troyan/builder/XMLReaderTest.java
@@ -1,13 +1,33 @@
 package by.troyan.builder;
 
+import by.troyan.entity.Candy;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 import static org.testng.Assert.*;
 
 public class XMLReaderTest {
 
-    @Test
-    public void testGetListOfCandiesByCertainParser() throws Exception {
+    XMLReader xmlReader = new XMLReader();
+    String filename = "file/candies.xml";
 
+    @Test
+    public void testGetListOfCandiesByCertainParserDOM() throws Exception {
+        List<Candy> candyList = xmlReader.getListOfCandiesByCertainParser(1, filename);
+        Assert.assertNotEquals(candyList, null);
+    }
+
+    @Test
+    public void testGetListOfCandiesByCertainParserSAX() throws Exception {
+        List<Candy> candyList = xmlReader.getListOfCandiesByCertainParser(2, filename);
+        Assert.assertNotEquals(candyList, null);
+    }
+
+    @Test
+    public void testGetListOfCandiesByCertainParserSTAX() throws Exception {
+        List<Candy> candyList = xmlReader.getListOfCandiesByCertainParser(3, filename);
+        Assert.assertNotEquals(candyList, null);
     }
 }
